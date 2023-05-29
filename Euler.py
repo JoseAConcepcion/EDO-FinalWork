61ee6f5979b28f236cccf290ffb044db3d3983ea
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-05-29 19:13:31 -0400

    delete rk4 duplicate

--- Euler.py
+++ Euler.py
@@ -1,16 +1,19 @@
 import numpy as np
 from scipy.integrate import RK45
 
-def euler(f , a, b, N, c1, c2):
+def euler(f1 , f2, f3, a, b, N, c1, c2, c3):
     
     h = (b-a)/N
     xs = np.arange(0,N,h)
     ys = np.zeros(len(xs))
     zs = np.zeros(len(ys))
-    xs = 0
+    xs[0] = c1
+    ys[0] = c2
+    zs[0] = c3
 
     for i in range(len(xs)):
+        
     
-        RK45()
-    return
+        
+    return xs, ys, zs
 
