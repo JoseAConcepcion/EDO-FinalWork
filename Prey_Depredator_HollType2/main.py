25c2c265b226f2e8dd74635301e748445daedbdd
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-06-02 03:39:40 -0400

    format fix

--- Prey_Depredator_HollType2/main.py
+++ Prey_Depredator_HollType2/main.py
@@ -1,19 +1,21 @@
 import modules.RK4 as rk4
 import modules.odes as odes
 import os
+
+
 def main() -> None:
 
     data = []
     # Read Params
     cwd = os.getcwd()
-    
-    with open( cwd + "/Prey_Depredator_HollType2/Simulations/S1.txt", "r") as file:
+
+    with open(cwd + "/Prey_Depredator_HollType2/Simulations/S1.txt", "r") as file:
         for line in file.readlines(-1):
             if(line != "\n"):
                 data.append([float(x) for x in line.split()])
 
     s1 = odes.Prey_Depredator_HollingTypeII(data[0])
-    
+
     c1, c2, c3 = data[1]
     t, x, y, z = rk4.runge_kutta_4(
         s1.f1, s1.f2, s1.f3, 0, c1, c2, c3, 1e-8, 10000)
