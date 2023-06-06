52f0c3c0f2fbda89673ceb686663ff385ee0ee0f
JoseAConcepcion <josea.concepcion@estudiantes.matcom.uh.cu>
2023-06-06 13:32:36 -0400

    changes

--- Prey_Depredator_HollType2/main.py
+++ Prey_Depredator_HollType2/main.py
@@ -1,15 +1,19 @@
 import modules.RK4 as rk4
 import modules.odes as odes
 import os
-
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+from sympy import symbol
 
 def main() -> None:
+
 
     data = []
     # Read Params
     cwd = os.getcwd()
 
-    with open(cwd + "/Prey_Depredator_HollType2/Simulations/S1.txt", "r") as file:
+    with open(cwd + "/Prey_Depredator_HollType2/Simulations/S6.txt", "r") as file:
         for line in file.readlines(-1):
             if(line != "\n"):
                 data.append([float(x) for x in line.split()])
@@ -17,15 +21,47 @@ def main() -> None:
     s1 = odes.Prey_Depredator_HollingTypeII(data[0])
 
     c1, c2, c3 = data[1]
-    t, x, y, z = rk4.runge_kutta_4(
-        s1.f1, s1.f2, s1.f3, 0, c1, c2, c3, 1e-8, 10000)
+    t = np.linspace(0, 1000, 1000)
+    # t, x, y, z = rk4.runge_kutta_4(
+    #     s1.f1, s1.f2, s1.f3, 0, c1, c2, c3, 0.001, 1000000)
+    def system(v,t):
+        x,y,z = v
+        return [s1.f1(t,x,y,z), s1.f2(t,x,y,z), s1.f3(t,x,y,z)]
+    sol = odeint(system, [c1,c2,c3], t)
 
     limit = 50
 
-    for i in range(limit): # tiempo , x, y, z
-        print(t[i], x[i], y[i], z[i])
+    # for i in range(limit): # tiempo , x, y, z
+    #     print(t[i], x[i], y[i], z[i])
 
+
+    plt.plot(t, sol[0:,0], label="x")
+    plt.plot(t, sol[0:, 1], label="y")
+    plt.plot(t, sol[0:, 2], label="z")
+    plt.legend()
+    plt.show()
     return
 
+def find_initial():
+    a = np.array([[2 , -2, 3], [1,1,1], [1,3,-1]])
+    b = np.array([1, 2, 3])
+    x = np.linalg.solve(a, b)
+    print(x)
+    return x
 
-main()
+def main2():
+    c1,c2,c3 = find_initial()
+    def f1(t, x,y,z):
+        return 2*x-2*y+3*z
+    def f2(t, x,y,z):
+        return x+y+z
+    def f3(t, x,y,z):
+        return x+3*y-z
+
+    def system(v , t):
+        x,y,z = v
+        return [f1(t,x,y,z), f2(t,x,y,z), f3(t,x,y,z)]
+    
+    t = np.arange((0, 10, 0.01))
+    sol = odeint(system, [c1,c2,c3], t)
+
--- Prey_Depredator_HollType2/modules/odes.py
+++ Prey_Depredator_HollType2/modules/odes.py
@@ -1,57 +1,156 @@
 class Prey_Depredator_HollingTypeII:
 
-    # Parametros
-    r = 0
-    beta = 0
-    alpha = 0
-    alpha1 = 0
-    eta = 0
-    eta1 = 0
-    k = 0
-    rho = 0
-    m = 0
-    mu = 0
+    # # Parametros
+    # r = 0
+    # beta = 0
+    # alpha = 0
+    # alpha1 = 0
+    # eta = 0
+    # eta1 = 0
+    # k = 0
+    # rho = 0
+    # m = 0
+    # mu = 0
 
-    def __init__(self, params):
-        # [r, alpha, beta, rho, m, mu, eta, alpha1, eta1, k]
-        # lo ideal era hacer el vector con los parametros pero es ilegible en las formulas
-        self.r = params[0]
-        self.beta = params[1]
-        self.alpha = params[2]
-        self.alpha1 = params[3]
-        self.eta = params[4]
-        self.eta1 = params[5]
-        self.k = params[6]
-        self.rho = params[7]
-        self.m = params[8]
-        self.mu = params[9]
+    # def __init__(self, params):
+    #     # [r, alpha, beta, rho, m, mu, eta, alpha1, eta1, k]
+    #     # lo ideal era hacer el vector con los parametros pero es ilegible en las formulas
+    #     self.r = params[0]
+    #     self.beta = params[1]
+    #     self.alpha = params[2]
+    #     self.alpha1 = params[3]
+    #     self.eta = params[4]
+    #     self.eta1 = params[5]
+    #     self.k = params[6]
+    #     self.rho = params[7]
+    #     self.m = params[8]
+    #     self.mu = params[9]
         
+    # # EDO
+    # def f1(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.r*x * (1 - x/self.k) - self.beta*x - self.alpha*x*z
+
+    # def f2(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.beta*x - self.eta*y*z/(self.m + y) - self.mu*y
+
+    # def f3(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.alpha1*x*z - self.rho*z*z - self.eta1*y*z/(self.m + y)
+
+    # def __str__(self) -> str:
+    #     string = "Parametros\n"
+    #     string += "r = " + str(self.r) + "\n"
+    #     string += "alpha = " + str(self.alpha) + "\n"
+    #     string += "beta = " + str(self.beta) + "\n"
+    #     string += "rho = " + str(self.rho) + "\n"
+    #     string += "m = " + str(self.m) + "\n"
+    #     string += "mu = " + str(self.mu) + "\n"
+    #     string += "eta = " + str(self.eta) + "\n"
+    #     string += "alpha1 = " + str(self.alpha1) + "\n"
+    #     string += "eta1 = " + str(self.eta1) + "\n"
+    #     string += "k = " + str(self.k) + "\n"
+
+    #     string += "\nEDO\n"
+    #     string += "dx/dt = " + "{0}*x*(1-x/{1}) - {2}*x - {3}*x*z".format(self.r,self.k,self.beta,self.alpha) + "\n"
+    #     string += "dy/dt = " + "{0}*x - {1}*y*z/({2} + y) - {3}*y".format(self.beta,self.rho,self.rho,self.mu) + "\n"
+    #     string += "dz/dt = " + "{0}*x*z - {1}*z^2 - {2}*y*z/(y + {3})".format(self.alpha1,self.rho,self.eta1, self.m) + "\n"
+
+    #     return string
+    # r = 0
+    # beta = 0
+    # alpha = 0
+    # alpha1 = 0
+    # eta = 0
+    # eta1 = 0
+    # k = 0
+    # rho = 0
+    # m = 0
+    # mu = 0
+
+    # def __init__(self, params):
+    #     # [r, alpha, beta, rho, m, mu, eta, alpha1, eta1, k]
+    #     # lo ideal era hacer el vector con los parametros pero es ilegible en las formulas
+    #     self.r = params[0]
+    #     self.beta = params[1]
+    #     self.alpha = params[2]
+    #     self.alpha1 = params[3]
+    #     self.eta = params[4]
+    #     self.eta1 = params[5]
+    #     self.k = params[6]
+    #     self.rho = params[7]
+    #     self.m = params[8]
+    #     self.mu = params[9]
+
+    # # EDO
+    # def f1(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.r*x * (1 - x/self.k) - self.beta*x - self.alpha*x*z
+
+    # def f2(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.beta*x - self.eta*y*z/(self.m + y) - self.mu*y
+
+    # def f3(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.alpha1*x*z - self.rho*z*z - self.eta1*y*z/(self.m + y)
+
+    # def __str__(self) -> str:
+    #     string = "Parametros\n"
+    #     string += "r = " + str(self.r) + "\n"
+    #     string += "alpha = " + str(self.alpha) + "\n"
+    #     string += "beta = " + str(self.beta) + "\n"
+    #     string += "rho = " + str(self.rho) + "\n"
+    #     string += "m = " + str(self.m) + "\n"
+    #     string += "mu = " + str(self.mu) + "\n"
+    #     string += "eta = " + str(self.eta) + "\n"
+    #     string += "alpha1 = " + str(self.alpha1) + "\n"
+    #     string += "eta1 = " + str(self.eta1) + "\n"
+    #     string += "k = " + str(self.k) + "\n"
+
+    #     string += "\nEDO\n"
+    #     string += "dx/dt = " + "{0}*x*(1-x/{1}) - {2}*x - {3}*x*z".format(self.r,self.k,self.beta,self.alpha) + "\n"
+    #     string += "dy/dt = " + "{0}*x - {1}*y*z/({2} + y) - {3}*y".format(self.beta,self.rho,self.rho,self.mu) + "\n"
+    #     string += "dz/dt = " + "{0}*x*z - {1}*z^2 - {2}*y*z/(y + {3})".format(self.alpha1,self.rho,self.eta1, self.m) + "\n"
+
+    #     return string
+    
+    # Parametros new
+
+    a1 = 0
+    a2 = 0
+    b2 = 0
+    c = 0
+    d = 0
+    k = 0
+    sigma = 0
+    ro = 0
+    beta = 0
+
+    def __init__(self, params): 
+        self.a1 = params[0]
+        self.a2 = params[1]
+        self.b2 = params[2]
+        self.c = params[3]
+        self.d = params[4]
+        self.k = params[5]
+        self.sigma = params[6]
+        self.ro = params[7]
+        self.beta = params[8]
+
+
+
+    # EDO
+    # def f1(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.ro*x * (1 - x/self.k) - self.a1*x*y
+
+    # def f2(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.c*self.a1*x*y - self.d*y - ((self.a2*y*z)/(y + self.b2))
+
+    # def f3(self, t: float, x: float, y: float, z: float) -> float:
+    #     return self.sigma*z*z - ((self.beta*z*z)/(y + self.b2))
+
     # EDO
     def f1(self, t: float, x: float, y: float, z: float) -> float:
-        return self.r*x * (1 - x/self.k) - self.beta*x - self.alpha*x*z
+        return 2*x - 2*y + 3*z
 
     def f2(self, t: float, x: float, y: float, z: float) -> float:
-        return self.beta*x - self.eta*y*z/(self.m + y) - self.mu*y
+        return 1*x + y + 1*z
 
     def f3(self, t: float, x: float, y: float, z: float) -> float:
-        return self.alpha1*x*z - self.rho*z*z - self.eta1*y*z/(self.m + y)
-
-    def __str__(self) -> str:
-        string = "Parametros\n"
-        string += "r = " + str(self.r) + "\n"
-        string += "alpha = " + str(self.alpha) + "\n"
-        string += "beta = " + str(self.beta) + "\n"
-        string += "rho = " + str(self.rho) + "\n"
-        string += "m = " + str(self.m) + "\n"
-        string += "mu = " + str(self.mu) + "\n"
-        string += "eta = " + str(self.eta) + "\n"
-        string += "alpha1 = " + str(self.alpha1) + "\n"
-        string += "eta1 = " + str(self.eta1) + "\n"
-        string += "k = " + str(self.k) + "\n"
-
-        string += "\nEDO\n"
-        string += "dx/dt = " + "{0}*x*(1-x/{1}) - {2}*x - {3}*x*z".format(self.r,self.k,self.beta,self.alpha) + "\n"
-        string += "dy/dt = " + "{0}*x - {1}*y*z/({2} + y) - {3}*y".format(self.beta,self.rho,self.rho,self.mu) + "\n"
-        string += "dz/dt = " + "{0}*x*z - {1}*z^2 - {2}*y*z/(y + {3})".format(self.alpha1,self.rho,self.eta1, self.m) + "\n"
-
-        return string
+        return x + 3*y - z 
