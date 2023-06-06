dc9abe566f83b000580f272e0364aeabf7a142ac
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-06-06 14:09:32 -0400

    ""

--- Prey_Depredator_HollType2/main.py
+++ Prey_Depredator_HollType2/main.py
@@ -2,10 +2,14 @@ import modules.metodos_numericos as edo
 import modules.odes as odes
 import os
 from math import *
+import numpy as np
+import scipy as sp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 
-def main() -> None:
 
+def read_data() -> list:
     data = []
     # Read Params
     cwd = os.getcwd()
@@ -15,11 +19,14 @@ def main() -> None:
             if(line != "\n"):
                 data.append([float(x) for x in line.split()])
 
-    s1 = odes.prey_depredator_hollingTypeII(data[0])
+    return data
 
-    
-    iteraciones = 100000
-    h = 1e-2
+def main() -> None:
+    data = read_data()    
+
+    s1 = odes.prey_depredator_hollingTypeII(data[0])
+    iteraciones = 1000
+    h = 0.1
     c1, c2, c3 = data[1]
     
     t, x, y, z = edo.runge_kutta_4(
@@ -30,7 +37,62 @@ def main() -> None:
     for i in range(limit): # tiempo , x, y, z
             index = i*50
             print(t[index], x[index], y[index], z[index])
+
+    plt.plot(t, x, label = 'presas1')
+    plt.plot(t, y, label = 'presas2')
+    plt.plot(t, z, label = 'depredadores')
+    plt.legend()
+    plt.show()
+    return 
+
+def main2() -> None:
+    data = read_data()
+    s1 = odes.prey_depredator_hollingTypeII(data[0])
+    c = data[1]
+    
+    def system(vector , time):
+        x,y,z = vector
+        t = time
+        return s1.f1(t,x,y,z), s1.f2(t,x,y,z), s1.f3(t,x,y,z)    
+    
+    t = np.arange(0,100,0.1)
+    sol = odeint(system, c, t)
+    plt.plot(t, sol[:,0], label = 'presas1')
+    plt.plot(t, sol[:,1], label = 'presas2')
+    plt.plot(t, sol[:,2], label = 'depredadores')
+    plt.legend()
+    plt.show()
+
     return
 
+def main3() -> None:
+    data = read_data()
+    s1 = odes.prey_depredator_hollingTypeII(data[0])
+    c = data[1]
+    
+    def system(time, vector):
+        x,y,z = vector
+        t = time
+        return np.array([s1.f1(t,x,y,z), s1.f2(t,x,y,z), s1.f3(t,x,y,z)])    
+    
+    t, sol = edo.runge_kutta(system, c, 0.1, 1000)
 
-main()
+    prey1 = []
+    prey2 = []
+    depredator = []
+    for i in range(len(sol)):
+        prey1.append(sol[i][0])
+        prey2.append(sol[i][1])
+        depredator.append(sol[i][2]) 
+
+    plt.plot(t, prey1, label = 'presas1')
+    plt.plot(t, prey2, label = 'presas2')
+    plt.plot(t, depredator, label = 'depredadores')
+    plt.legend()
+    plt.show()
+
+    return
+
+main() # -> Ejecutar el metodo de Runge-Kutta 4,(old)
+#main2() # -> Ejecutar el metodo de Runge-Kutta 4,(python)
+main3()
--- Prey_Depredator_HollType2/modules/metodos_numericos.py
+++ Prey_Depredator_HollType2/modules/metodos_numericos.py
@@ -47,9 +47,23 @@ def runge_kutta_4(
         l4 = f2(t + h, x + h*k3, y + h*l3, z + h*m3)
         m4 = f3(t + h, x + h*k3, y + h*l3, z + h*m3)
 
-        x, y, z, t = x + h/6*(k1 + 2*k2 + 2*k3 + k4), y + h/6 * \
-            (l1 + 2*l2 + 2*l3 + l4), z + h/6*(m1 + 2*m2 + 2*m3 + m4), t + h
+        x, y, z, t = x + h/6*(k1 + 2*k2 + 2*k3 + k4), y + h/6 * (l1 + 2*l2 + 2*l3 + l4), z + h/6*(m1 + 2*m2 + 2*m3 + m4), t + h
 
         xs.append(x), ys.append(y), zs.append(z), ts.append(t)
 
     return ts, xs, ys, zs
+
+def runge_kutta(f , c , h, n): # f sistema, c condiciones iniciales
+    x = [np.array(c)]
+    t = [np.array(0)]
+
+    for i in range(n):
+        k1 = f(t[i] , x[i])
+        k2 = f(t[i] + h/2, x[i] + 0.5*h*k1)
+        k3 = f(t[i] + h/2, x[i] + 0.5*h*k2)
+        k4 = f(t[i] + h, x[i] + h*k3)
+        t.append(t[i] + h)
+        x.append(x[i] + h/6*(k1 + 2*k2 + 2*k3 + k4))
+    
+    
+    return t, x
