422b4b8224d4b9d083d03ae9cd85f37ef1c3c89b
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-06-10 00:59:36 -0400

    read data ready

--- Prey_Depredator_HollType2/main.py
+++ Prey_Depredator_HollType2/main.py
@@ -2,24 +2,36 @@ import modules.metodos_numericos as edo
 import modules.odes as odes
 import numpy as np
 from math import *
-
+import glob
 import os
 
-def read_data(path) -> list:
-    data = []
-    # Read Params
-    cwd = os.getcwd()
 
-    with open(cwd + "/Prey_Depredator_HollType2/Simulations/S1.txt", "r") as file:
-        for line in file.readlines():
-            if(line != "\n"):
-                data.append([float(x) for x in line.split()])
+def read_data(folder_path: str, extension: str) -> list:
+    data = []
+    files = glob.glob(folder_path + "*.{}".format(extension))
+
+    # Read
+    for file in files:
+        current_data = []
+        with open(file, "r") as f:
+            for line in f.readlines():
+                if(line != "\n"):
+                    current_data.append([float(x) for x in line.split()])
+        if len(current_data) != 0:
+            data.append(current_data)
 
     return data
 
 
+def main() -> None:
+    data = read_data(
+        os.getcwd() + "/Prey_Depredator_HollType2/Simulations/", "txt")
+
+    return
+
+
 def print_jacobian_symbolic() -> None:
-    
+
     jacobian = odes.prey_depredator_hollingTypeII.jacobi_matrix_symbolic
     print('- - '*30)  # separador
     representation = "\n"
@@ -29,5 +41,8 @@ def print_jacobian_symbolic() -> None:
         representation += '\n'
 
     print(representation + '\n')  # para visualizar la matriz
-    
+
     return
+
+
+main()
